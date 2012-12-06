a9f8d4191fc506ff5ad53b1f73852b36c54abe29
amuxtux <amitsinghai.jain@gmail.com>
2012-12-06 08:00:42 +0100

    added more examples

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ var gm = require('gm')
   , dir = __dirname + '/imgs'
 
 gm(dir + "/original.jpg")
-  .crop(140,100)
-  .background("#FF0000")
-  .extent(340,300)
+  //.crop(140,100)
+  .background("#FF0000")  //red background
+  .extent(480,300)
   .write(dir + '/background.jpg', function (err) {
     if (err) return console.dir(arguments)
     console.log(this.outname + " created  ::  " + arguments[3])
